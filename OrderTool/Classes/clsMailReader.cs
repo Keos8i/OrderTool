eeea03be7a85adec559701196ec22df1ec74fb86
Colin Eikis-Sagcob <c.eikissagcob@gmail.com>
2020-11-09 23:06:44 +0100

    clsMailReader, clsMailOrder

---
+++ OrderTool/Classes/clsMailReader.cs
@@ -0,0 +1,102 @@
+﻿using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderTool.Classes
+{
+    class clsMailReader
+    {
+
+        #region Public Properties
+
+        #endregion
+
+        #region Private Properties
+
+        private string MailServer { get; set; }
+        private string UserLogin { get; set; }
+        private string UserPassword { get; set; }
+        private int Port { get; set; }
+
+
+
+        #endregion
+
+        #region Constructor 
+        clsMailReader(string pMailServer, string pUserLogin, string pUserPassword, int pPort) 
+        {
+            this.MailServer = pMailServer;
+            this.UserLogin = pUserLogin;
+            this.UserPassword = pUserPassword;
+            this.Port = pPort;
+        }
+        #endregion
+
+        #region Public Methods
+
+        public void GetAmazonOrders() 
+        {
+            // Get the Order Mail via IMAP
+
+            using (var client = new ImapClient()) 
+            {
+                // Remove this token because I dont have it.
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                // Login
+                client.Authenticate(this.UserLogin, this.UserPassword);
+
+                // Get Inbox
+                var inbox = client.Inbox;
+                inbox.Open(FolderAccess.ReadOnly);
+
+                // Filter Mails
+                var results = inbox.Search(SearchOptions.All, SearchQuery.FromContains("bestellbestaetigung@amazon.de"));
+
+                // Get the results
+
+                List<MimeMessage> messages = new List<MimeMessage>();
+
+                foreach (var mailId in results.UniqueIds)
+                {
+                    MimeMessage message = inbox.GetMessage(mailId);
+                    messages.Add(message);
+                }
+
+                // Create OrderObjects
+                List<clsOrder> orders = new List<clsOrder>();
+
+                foreach (MimeMessage message in messages)
+                {
+                    //clsOrder order = new clsOrder(
+                    //    message.MessageId,
+                    //    message.Body,
+                    //    message.Subject,
+                    //    message.Subject.Split('"').Last().TrimEnd('"'),
+                    //
+                    //) ;
+
+                    orders.Add(order);
+                }
+                
+
+                // Disconnect client
+                client.Disconnect(true);
+            }
+
+        }
+
+        #endregion
+
+        #region Private Methods
+        #endregion
+
+    }
+}
---
+++ OrderTool/Classes/clsOrder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderTool.Classes
+{
+    public class clsOrder
+    {
+
+        #region Public Properties
+
+        int OrderId { get; set; }
+        string OrderMailBody { get; set; }
+        string OrderMailSubject { get; set; }
+        string OrderItemName { get; set; }
+        DateTime? OrderDeliveryDate { get; set; }
+        bool? PremiumDelivery { get; set; }
+        string OrderNumber { get; set; }
+        DateTime OrderMailImport { get; set; }
+
+        #endregion
+
+        #region Constructor
+        public clsOrder(int pOrderId, string pOrderMailBody, string pOrderMailSubject, string pOrderItemName, 
+            DateTime pOrderDeliveryDate, bool pPremiumDelivery, string pOrderNumber) 
+        {
+            this.OrderId = pOrderId;
+            this.OrderMailBody = pOrderMailBody;
+            this.OrderMailSubject = pOrderMailSubject;
+            this.OrderItemName = pOrderItemName;
+            this.OrderDeliveryDate = pOrderDeliveryDate;
+            this.PremiumDelivery = pPremiumDelivery;
+            this.OrderNumber = pOrderNumber;
+            this.OrderMailImport = DateTime.Now;
+        }
+
+        public clsOrder(){}
+        #endregion
+    }
+}
